ab31f5b1a4a3e944d178f2971eef751235a864b8
lovelife10000 <1358180015@qq.com>
2017-09-08 17:41:04 +0800

    create unhappy-bin

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-
- 'use strict';
+'use strict';
 const path = require('path');
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
-const app =new Koa();
- const router = new KoaRouter();
- const unhappyCore = require('unhappy-core');
- // router.get('/', function (ctx, next) {
- //   ctx.body = 'Hello World!';
- // });
-require('./app/router')(router);
- app
-   .use(router.routes())
-   .use(router.allowedMethods());
+const app = new Koa();
+const router = new KoaRouter();
 
+router.get('/', function (ctx, next) {
+  ctx.body = 'Hello World!';
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 
 app.listen(3000);
 
+console.log(process.pid);
