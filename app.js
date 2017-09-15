6ba44328053e4b8ce1f3586c819e3d2cdc1329f0
lovelife10000 <1358180015@qq.com>
2017-09-15 18:21:20 +0800

    cluster-client 分析

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-'use strict';
-const path = require('path');
-const Koa = require('koa');
-const KoaRouter = require('koa-router');
-const app = new Koa();
-const router = new KoaRouter();
-
-router.get('/', function (ctx, next) {
-  ctx.body = 'Hello World!';
+#!/usr/bin/env node
+// 'use strict';
+// const path = require('path');
+// const Koa = require('koa');
+// const KoaRouter = require('koa-router');
+// const app = new Koa();
+// const router = new KoaRouter();
+//
+// router.get('/', function (ctx, next) {
+//   ctx.body = 'Hello World!';
+// });
+//
+// app
+//   .use(router.routes())
+//   .use(router.allowedMethods());
+//
+//
+// app.listen(3000);
+//
+// console.log(process.pid);
+ const co = require('co');
+console.log(6);
+co(function* aa() {
+  console.log(7);
+  const ye=yield b();
+  const ye2=yield d();
+  console.log(ye);
 });
+function* b() {
+  return c=>{
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
-
-
-app.listen(3000);
-
-console.log(process.pid);
+    console.log(2);
+  };
+}
+function* d() {
+  console.log('d');
+}
