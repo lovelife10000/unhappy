de44a256a86506962799c4bbc9d305d9b6c8f087
lovelife10000 <1358180015@qq.com>
2017-09-20 18:40:57 +0800

    next in kao analysis

--- app.js
+++ app.js
@@ -35,11 +35,11 @@
 // function* d() {
 //   console.log('d');
 // }
-const debug = require('debug')('cluster-client:server');
-const cluster = require('cluster-client');
-const net = require('net');
-const co = require('co');
-const fs = require('fs');
+// const debug = require('debug')('cluster-client:server');
+// const cluster = require('cluster-client');
+// const net = require('net');
+// const co = require('co');
+// const fs = require('fs');
 // co(function* () {
 //   yield cl();
 //   return 6;
@@ -75,55 +75,72 @@ const fs = require('fs');
 
 
 
-const server = net.createServer((c) => {
-  // 'connection' listener
-  console.log('client connected');
-  c.on('end', () => {
-    console.log('client disconnected');
-  });
-  c.write('hello\r\n');
-  c.pipe(c);
-});
-server.on('error', (err) => {
-  throw err;
-});
+// const server = net.createServer((c) => {
+//   // 'connection' listener
+//   console.log('client connected');
+//   c.on('end', () => {
+//     console.log('client disconnected');
+//   });
+//   c.write('hello\r\n');
+//   c.pipe(c);
+// });
+// server.on('error', (err) => {
+//   throw err;
+// });
 // server.listen(8124, () => {
 //   console.log('server bound');
 // });
 // debug('%s',server.listen);
-server.listen({
-  port:8124,
-  host: '127.0.0.1',
-  // When exclusive is true, the handle is not shared, and attempted port sharing results in an error.
-  exclusive: true,
-},function () {
-  debug('server.listen回调');
-});
-co(function* () {
-  console.log(88);
-  yield ly();
+// server.listen({
+//   port:8124,
+//   host: '127.0.0.1',
+//   // When exclusive is true, the handle is not shared, and attempted port sharing results in an error.
+//   exclusive: true,
+// },function () {
+//   debug('server.listen回调');
+// });
+// co(function* () {
+//   console.log(88);
+//   yield ly();
+//   console.log(66);
+// });
+// console.log(77);
+// function ly() {
+//   let  _readyCallbacks=[];
+//   return new Promise((resolve, reject) => {
+//     debug('sdk-base中的ready3');
+//
+//     _readyCallbacks.push(err => {
+//       if (err) {
+//         debug('sdk-base中的ready336');
+//         reject(err);
+//       } else {
+//         debug('sdk-base中的ready337');
+//         resolve();
+//       }
+//     });
+//     debug('sdk-base中的ready339');
+//   });
+// }
+// server.on('listening', () => {
+//   console.log('server bound2');
+// });
+//
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+
+  // 发送 HTTP 头部
+  // HTTP 状态值: 200 : OK
+  // 内容类型: text/plain
   console.log(66);
-});
-console.log(77);
-function ly() {
-  let  _readyCallbacks=[];
-  return new Promise((resolve, reject) => {
-    debug('sdk-base中的ready3');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
 
-    _readyCallbacks.push(err => {
-      if (err) {
-        debug('sdk-base中的ready336');
-        reject(err);
-      } else {
-        debug('sdk-base中的ready337');
-        resolve();
-      }
-    });
-    debug('sdk-base中的ready339');
-  });
-}
-server.on('listening', () => {
-  console.log('server bound2');
-});
+  // 发送响应数据 "Hello World"
+  response.end('Hello World\n');
+}).listen(8888);
 
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
 
--- config/plugin.js
+++ config/plugin.js
@@ -1,4 +1,75 @@
 'use strict';
 
-// had enabled by egg
-// exports.static = true;
+module.exports = {
+
+  onerror: {
+    enable: true,
+    package: 'unhappy-onerror',
+  },
+
+
+  session: {
+    enable: true,
+    package: 'unhappy-session',
+  },
+
+
+  i18n: {
+    enable: true,
+    package: 'unhappy-i18n',
+  },
+
+
+  watcher: {
+    enable: true,
+    package: 'unhappy-watcher',
+  },
+
+
+  multipart: {
+    enable: true,
+    package: 'unhappy-multipart',
+  },
+
+
+  security: {
+    enable: true,
+    package: 'unhappy-security',
+  },
+
+
+  development: {
+    enable: true,
+    package: 'unhappy-development',
+  },
+
+
+  logrotator: {
+    enable: true,
+    package: 'unhappy-logrotator',
+  },
+
+
+  schedule: {
+    enable: true,
+    package: 'unhappy-schedule',
+  },
+
+
+  static: {
+    enable: true,
+    package: 'unhappy-static',
+  },
+
+
+  jsonp: {
+    enable: true,
+    package: 'unhappy-jsonp',
+  },
+
+
+  view: {
+    enable: true,
+    package: 'unhappy-view',
+  },
+};
